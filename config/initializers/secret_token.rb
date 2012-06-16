38e9e366d2375388172c1f7c0c0e9a749e96ee6f
Shoaib Burq <saburq@gmail.com>
2012-06-16 19:32:34 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Embertutorial::Application.config.secret_token = 'ad5552f920a3341f6ef043c3c7c334bc0ffc6e19ce7b48661c320c0526fd34576865738951a76c764ffa1d9f022d117032205c021ae0742508b91d866cae2da6'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Embertutorial::Application.routes.draw do
+  root :to => "home#index"
+end
